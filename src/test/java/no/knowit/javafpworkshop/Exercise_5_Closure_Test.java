80974d34eb2aad695e0682e3672f91a7b662880e
Fredrik Vraalsen <fredrik@vraalsen.no>
2012-04-26 22:06:27 +0200

    Added closure exercise

---
+++ src/test/java/no/knowit/javafpworkshop/Exercise_5_Closure_Test.java
@@ -0,0 +1,133 @@
+package no.knowit.javafpworkshop;
+
+import static java.util.Collections.emptyList;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+import com.google.common.collect.ImmutableList;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import org.joda.time.LocalDate;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Hint: The imports below are useful for solving the exercises
+ */
+
+import static fj.data.List.iterableList;
+
+import fj.F;
+import fj.F2;
+
+public class Exercise_5_Closure_Test {
+
+	private static class ProductRepository {
+
+		private final List<Product> products;
+
+		public ProductRepository(Product... products) {
+			this.products = ImmutableList.copyOf(products);
+		}
+
+		/**
+		 * Exercise 5: Return the list of products that are available on the
+		 * given date. Implement this method using FunctionalJava. You may
+		 * create helper functions and classes.
+		 * 
+		 * Hint: Check Functional Java imports at the top of the source file.
+		 */
+		public Collection<Product> getAvailableProducts(final LocalDate date) {
+			return emptyList();
+		}
+
+	}
+
+	/*
+	 * Test code below here
+	 */
+
+	@Test
+	@Ignore
+	public void empty_list_when_no_products() {
+		ProductRepository productRepo = new ProductRepository();
+		Collection<Product> available = productRepo.getAvailableProducts(LocalDate.now());
+		assertThat(available.isEmpty(), is(equalTo(true)));
+	}
+
+	@Test
+	@Ignore
+	public void product_available_when_matching_date() {
+		LocalDate today = LocalDate.now();
+		Product coolProduct = new Product("Cool product", today, today);
+		ProductRepository productRepo = new ProductRepository(coolProduct);
+
+		Collection<Product> available = productRepo.getAvailableProducts(today);
+		assertThat(available.size(), is(equalTo(1)));
+		assertThat(available, hasItems(coolProduct));
+	}
+
+	@Test(expected = UnsupportedOperationException.class)
+	@Ignore
+	public void cannot_remove_elements_from_available_products() {
+		LocalDate today = LocalDate.now();
+		Product coolProduct = new Product("Cool product", today, today);
+		ProductRepository productRepo = new ProductRepository(coolProduct);
+
+		Collection<Product> available = productRepo.getAvailableProducts(today);
+		Iterator<Product> i = available.iterator();
+		i.next();
+		i.remove();
+		fail("Should not be allowed to remove elements from an immutable list");
+	}
+
+	@Test
+	@Ignore
+	public void empty_list_when_no_products_matching_date() {
+		LocalDate today = LocalDate.now();
+		LocalDate yesterday = today.minusDays(1);
+		Product discontinuedProduct = new Product("Discontinued product", yesterday, yesterday);
+
+		ProductRepository productRepo = new ProductRepository(discontinuedProduct);
+
+		Collection<Product> available = productRepo.getAvailableProducts(today);
+		assertThat(available.isEmpty(), is(equalTo(true)));
+	}
+
+	@Test
+	@Ignore
+	public void find_discontinued_products() {
+		LocalDate today = LocalDate.now();
+		LocalDate yesterday = today.minusDays(1);
+		Product discontinuedProduct = new Product("Discontinued product", yesterday, yesterday);
+
+		ProductRepository productRepo = new ProductRepository(discontinuedProduct);
+
+		Collection<Product> available = productRepo.getAvailableProducts(yesterday);
+		assertThat(available, hasItems(discontinuedProduct));
+	}
+
+	@Test
+	@Ignore
+	public void find_discontinued_then_new_products() {
+		LocalDate today = LocalDate.now();
+		LocalDate yesterday = today.minusDays(1);
+		Product discontinuedProduct = new Product("Discontinued product", yesterday, yesterday);
+		Product newProduct = new Product("New product", today, null);
+
+		ProductRepository productRepo = new ProductRepository(discontinuedProduct, newProduct);
+
+		Collection<Product> availableYesterday = productRepo.getAvailableProducts(yesterday);
+		assertThat(availableYesterday, hasItems(discontinuedProduct));
+
+		Collection<Product> availableToday = productRepo.getAvailableProducts(today);
+		assertThat(availableToday, hasItems(newProduct));
+	}
+
+
+}
